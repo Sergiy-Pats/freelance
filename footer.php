bd9c44e1f7a97b783c4c698192be8ddfe72c8401
Sirgey-Pats <sirgey.patsora@gmail.com>
2021-04-27 19:45:28 +0300

    sadasdasd

---
+++ footer.php
@@ -0,0 +1,13 @@
+<footer class="footer">
+			<div class="container footer__body">
+				<div class="footer__left">
+					<div class="footer__copyright">2021© При использовании материалов ссылка на сайт обязательна</div>
+					<div class="footer__links links-footer">
+						<a href="#" class="links-footer__link"><span>О нас</span></a>
+						<a href="#" class="links-footer__link"><span>Контакты</span></a>
+						<a href="#" class="links-footer__link"><span>Академия</span></a>
+					</div>
+				</div>
+
+			</div>
+		</footer>
---
+++ header.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+
+	<meta charset="utf-8">
+
+	<!--<meta name="viewport" content="width=1170"></!-->
+
+	<meta name="viewport" content="width=device-width,initial-scale=1.0,maximum-scale=1.0, user-scalable=0">
+
+	<link rel="stylesheet" type="text/css" href="css/style.css">
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.4/jquery.min.js"></script>
+	<!--
+	<script src="https://kit.fontawesome.com/7b2244a88d.js" crossorigin="anonymous"></script>
+-->
+	<script src="js/script.js"></script>
+	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+	<meta http-equiv="X-UA-Compatible" content="IE=edge" />
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="shortcut icon" href="https://bolgar.academy/favicon.ico" type="image/x-icon">
+
+
+	<link rel="stylesheet" type="text/css" href="https://bolgar.academy/web/css/slick.css" />
+	<link rel="stylesheet" type="text/css" href="https://bolgar.academy/web/css/fonts.css" />
+	<link rel="stylesheet" type="text/css" href="https://bolgar.academy/web/css/fontawesome-all.min.css" />
+	<link rel="stylesheet" type="text/css" href="https://bolgar.academy/web/css/bootstrap.min.css" />
+	<link rel="stylesheet" type="text/css" href="https://bolgar.academy/web/libs/fancybox/jquery.fancybox.min.css" />
+	<link rel="stylesheet" type="text/css" href="https://bolgar.academy/web/css/style.css?v1" />
+	<link rel="stylesheet" type="text/css" href="https://bolgar.academy/web/css/media.css?v1" />
+	<link rel="stylesheet" type="text/css" href="https://bolgar.academy/web/css/common.css?v1" />
+	<link rel="stylesheet" type="text/css" href="https://bolgar.academy/assets/6f91a93a/css/flags.css" />
+	<script type="text/javascript" src="https://bolgar.academy/assets/104e961/jquery.min.js"></script>
+
+
+	<title>Сведения об образовательной организации</title>
+	<meta name="description" content="PAges descr" />
+	<meta name="keywords" content="pages,keywords" />
+</head>
